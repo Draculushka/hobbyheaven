330ac485db31d1702f061e61c380040837882b67
Burkhanov MD <draculushkus@gmail.com>
2026-03-10 21:26:46 +0300

    теперь можно создавать, изменять, получать и удалять посты

--- app/main.py
+++ app/main.py
@@ -34,3 +34,33 @@ def get_posts(db: Session = Depends(get_db)):
     posts = db.query(Post).all()
     return posts
 
+@app.get("/posts/{post_id}", response_model=schemas.Post)
+def get_post(post_id: int, db: Session = Depends(get_db)):
+    post = db.query(Post).filter(Post.id == post_id).first()
+    return post
+
+@app.delete("/posts/{post_id}")
+def delete_post(post_id: int, db: Session = Depends(get_db)):
+    post = db.query(Post).filter(Post.id == post_id).first()
+
+    if post:
+        db.delete(post)
+        db.commit()
+        return {"message": "Post deleted"}
+
+    return {"error": "Post not found"}
+
+@app.put("/posts/{post_id}", response_model=schemas.Post)
+def update_post(post_id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
+    db_post = db.query(Post).filter(Post.id == post_id).first()
+
+    if db_post:
+        db_post.title = post.title
+        db_post.description = post.description
+
+        db.commit()
+        db.refresh(db_post)
+
+        return db_post
+
+    return {"error": "Post not found"}
